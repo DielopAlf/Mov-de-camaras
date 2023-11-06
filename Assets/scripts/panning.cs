772a7bd086ffa8cdaf3f34b9db803b7c5de0e7b4
DielopAlf <114227973+DielopAlf@users.noreply.github.com>
2023-11-06 08:50:42 +0000

    guardar

---
+++ Assets/scripts/panning.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class panning : MonoBehaviour
+{
+    public Transform targetObject; // El objeto hacia el cual la cámara dirigirá su vista
+    public float rotationSpeed = 2.0f;
+
+    private bool isPanning = true;
+
+    void Update()
+    {
+        if (isPanning)
+        {
+            // Calcula la dirección hacia el objetivo
+            Vector3 targetDirection = targetObject.position - transform.position;
+
+            // Calcula la rotación hacia el objetivo
+            Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
+
+            // Rota gradualmente hacia el objetivo
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        }
+    }
+}
